23cff8718992038272a16c9d58ecc0951280a1d9
marianaipmonteiro <mariana.ip.monteiro@gmail.com>
2021-08-18 21:14:11 +0100

    Add RandomPeriodicDisturbance system

---
+++ disturbance/RandomPeriodicDisturbance.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+from random import uniform
+from simulation.SimulatedSystem import SimulatedSystem
+
+
+class RandomPeriodicDisturbance(SimulatedSystem):
+    def __init__(self, fps = 5, variable = "Cb", min_disturbance = 0.0, max_disturbance = 5.0):
+        super().__init__(int(fps))
+        self.variable = variable
+        self.min_disturbance = float(min_disturbance)
+        self.max_disturbance = float(max_disturbance)
+
+    def step(self, time_delta: Decimal):
+        increment = Decimal(uniform(self.min_disturbance, self.max_disturbance))
+        self.apply_incremental_changes_to_latest_world({self.variable: increment})
